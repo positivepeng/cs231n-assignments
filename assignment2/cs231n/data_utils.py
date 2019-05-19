aeebbdb0c4723a647facae8c691f74c801baa8ea
positivepeng <903871772@qq.com>
2019-05-19 23:46:05 +0800

    update assignment2

--- assignment2/cs231n/data_utils.py
+++ assignment2/cs231n/data_utils.py
@@ -49,7 +49,7 @@ def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=1000,
     condensed to a single function.
     """
     # Load the raw CIFAR-10 data
-    cifar10_dir = r"E:\cs231n\assignment2\cifar-10-batches-py"
+    cifar10_dir = r"E:\cs231n\cs231n-assignments\assignment2\cifar-10-batches-py"
     X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
 
     # Subsample the data
